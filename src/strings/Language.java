39679cf46a9b64ee1e815bdc80d084623365196b
Fahid Shehzad <git@shehzad.eu>
2022-05-24 15:55:19 +0200

    Mehrsprachigkeit funktionalitat

---
+++ src/strings/Language.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package strings;
+
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author Fahid Shehzad
+ */
+public class Language {
+    private static final String LANGUAGE_RESOURCE_PFAD = "strings/language";
+    private static ResourceBundle language = ResourceBundle.getBundle(LANGUAGE_RESOURCE_PFAD);
+    
+    public static String getString(String key) {
+        System.out.println(language.getBaseBundleName());
+        String value = null;
+        if(!language.containsKey(key)) {
+            value = language.getString("StringNotFound");
+        } else {
+            value = language.getString(key);
+        }
+        return value;
+    }
+}
